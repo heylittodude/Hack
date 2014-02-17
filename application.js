0c82ef748d48e3359200a71f000c995f19afe695
Howard Xue <howard_xue@hotmail.com>
2014-02-17 14:38:54 -0800

    map canvas and location lookup added

---
+++ application.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	var geocoder;
+	var map;
+	function initialize() {
+		geocoder = new google.maps.Geocoder();
+		var latlng = new google.maps.LatLng(37.774929, -122.419416)
+		var mapOptions = {
+			zoom: 8,
+			center: latlng
+		}
+		map = new google.maps.Map(document.getElementById("map"), mapOptions)
+	}
+
+	function locateAddress() {
+		var address = document.getElementById('address').value;
+		geocoder.geocode( {'address': address}, function(result. status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				map.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map: map,
+					position: results[0].geometry.location
+				});
+			} else {
+				alert("Look up not successful, error reason: " + status);
+			}
+		});
+	}
+});
